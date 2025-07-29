2e04770237c987c7a37652b80f7b83f20f325bbf
Murillo Brito <murillosouza@live.com>
2025-07-29 17:40:03 -0300

    feat: alteracao de http

--- e2e/teste.spec.ts
+++ e2e/teste.spec.ts
@@ -2,14 +2,14 @@ import { test, expect } from '@playwright/test';
 import { spec } from 'node:test/reporters';
 
 test('Logo da Pagina', async ({ page }) => {
-  await page.goto('http://localhost:4200/home');
+  await page.goto('http://localhost:4200');
   const logo = page.getByRole('link', { name: 'Logo da aplicação Jornada' });
 
   await expect(logo).toBeVisible();
 });
 
 test('Botão de Login', async ({ page }) => {
-  await page.goto('http://localhost:4200/home');
+  await page.goto('http://localhost:4200');
   const loginBotao= page.getByTestId('botao-login')
 
   await expect(loginBotao).toBeVisible();
